a0b59eece091c73455e489ed8662b55a9c194a0d
Fauntle <the.kaempfer+fauntle@gmail.com>
2014-03-09 20:46:20 -0700

    temporarily center map immediately on SF

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -2,5 +2,6 @@ var $ = require('jquery');
 require('mapbox.js'); // attaches "automatically" to window.L
 
 $(function(){
-	L.mapbox.map( 'map', 'fauntleroy.hfnj60nk' );
+	var map = L.mapbox.map( 'map', 'fauntleroy.hfnj60nk' );
+	map.setView( [ 37.78, -122.419 ], 13 ); // temporary. This should eventually default
 });
