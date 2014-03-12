4ba10743e6fd7d04eed6094e31acd6c03177cfde
Fauntle <the.kaempfer+fauntle@gmail.com>
2014-03-11 23:30:44 -0700

    use lat/lng methods instead of directly accessing values

--- assets/scripts/mapView.js
+++ assets/scripts/mapView.js
@@ -40,8 +40,8 @@ module.exports = Backbone.View.extend({
 				bounds: this.geocoder_bounds
 			}, function( results, status ){
 				if( status !== 'OK' ) return cb( null, null );
-				var lat = results[0].geometry.location.d;
-				var lon = results[0].geometry.location.e;
+				var lat = results[0].geometry.location.lat();
+				var lon = results[0].geometry.location.lng();
 				return cb( null, [ lat, lon ] );
 			});
 		}.bind( this ), callback.bind( this ) );
