92b0309f038dd040c43f6330a227436b5cb47894
Fauntle <the.kaempfer+fauntle@gmail.com>
2014-03-13 08:31:59 -0700

    "paginate" through location results rather than limiting them

--- assets/scripts/locationsCollection.js
+++ assets/scripts/locationsCollection.js
@@ -5,19 +5,35 @@ var _ = require('underscore');
 
 module.exports = Backbone.Collection.extend({
 	url: function(){
-		// only do 8 at a time due to geocoding API restrictions
-		return SFDATA_API_URL +'?$q='+ this.query +'&$limit=8';
+		return SFDATA_API_URL +'?$q='+ this.query;
 	},
 	initialize: function(){
-		_.bindAll( this, 'fetchQuery' );
+		this.query;
+		this.start = 0;
+		_.bindAll( this, 'fetchQuery', 'next', 'prev' );
 		this.fetchQuery('');
 	},
 	fetchQuery: function( query ){
 		this.query = query;
+		this.start = 0;
 		this.fetch({
 			dataType: 'jsonp',
 			jsonp: '$jsonp',
 			reset: true
 		});
+	},
+	// set the next 5 locations as the current selection
+	next: function(){
+		if( this.length > this.start + 5 ){
+			this.start += 5;
+			this.trigger( 'start', this.start );
+		}
+	},
+	// set the previous 5 locations as the current selection
+	prev: function(){
+		if( this.start - 5 >= 0 ){
+			this.start -=5;
+			this.trigger( 'start', this.start );
+		}
 	}
 });
