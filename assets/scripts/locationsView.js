6a9cd945513a4eb692b89cdf149d1ca36e23d225
Fauntle <the.kaempfer+fauntle@gmail.com>
2014-03-13 23:58:29 -0700

    add next/previous navigation to locations view

--- assets/scripts/locationsView.js
+++ assets/scripts/locationsView.js
@@ -4,11 +4,23 @@ var $ = Backbone.$ = require('jquery');
 var locations_template = require('../templates/locations.hbs');
 
 module.exports = Backbone.View.extend({
+	events: {
+		'click a[href="#previous"]': 'clickPrevious',
+		'click a[href="#next"]': 'clickNext'
+	},
 	initialize: function(){
 		this.listenTo( this.collection, 'reset start', this.render );
 		this.render();
 	},
 	render: function(){
 		this.$el.html( locations_template( this.collection.current() ) );
+	},
+	clickPrevious: function( e ){
+		e.preventDefault();
+		this.collection.prev();
+	},
+	clickNext: function( e ){
+		e.preventDefault();
+		this.collection.next();
 	}
 })
