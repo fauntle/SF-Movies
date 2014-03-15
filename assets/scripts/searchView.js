ca18b523cba6c4bd156b14dc7453f1d4ff940454
Fauntle <the.kaempfer+fauntle@gmail.com>
2014-03-15 14:05:13 -0700

    use jquery.sparkartSuggest for autocomplete

--- assets/scripts/searchView.js
+++ assets/scripts/searchView.js
@@ -1,7 +1,12 @@
+var SFDATA_API_URL = '/api/v1/locations';
+
 var Backbone = require('backbone');
-var $ = Backbone.$ = require('jquery');
+var $ = jQuery = Backbone.$ = require('jquery');
+require('./vendor/jquery.sparkartSuggest.js');
+var _ = require('underscore');
 
 var search_template = require('../templates/search.hbs');
+var suggestion_template = require('../templates/suggestion.hbs');
 
 module.exports = Backbone.View.extend({
 	events: {
@@ -13,6 +18,21 @@ module.exports = Backbone.View.extend({
 	render: function(){
 		this.$el.html( search_template() );
 		this.$query = this.$('input[name="query"]');
+		this.$query.sparkartSuggest({
+			source: function( string, options, cb ){
+				$.getJSON( SFDATA_API_URL +'?q='+ string, function( suggestions ){
+					suggestions = _.uniq( suggestions, function( suggestion ){
+						return suggestion.title;
+					});
+					suggestions = _.pluck( suggestions, 'title' );
+					cb( suggestions );
+				});
+			},
+			elementConstructor: suggestion_template,
+			threshold: 1,
+			max: 5,
+			delay: 250
+		});
 	},
 	onSubmit: function( e ){
 		e.preventDefault();
