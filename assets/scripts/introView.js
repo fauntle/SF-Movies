841cecb6ca36184bc474cfa8cb3cf56aaf2dc8b5
Fauntle <the.kaempfer+fauntle@gmail.com>
2014-03-15 16:54:37 -0700

    add intro hiding functionality

--- assets/scripts/introView.js
+++ assets/scripts/introView.js
@@ -1,18 +1,21 @@
 var Backbone = require('backbone');
 var $ = Backbone.$ = require('jquery');
-var _ = require('underscore');
 
 var intro_template = require('../templates/intro.hbs');
 
 module.exports = Backbone.View.extend({
+	events: {
+		'click a[href="#gotit"]': 'clickGotIt'
+	},
 	initialize: function(){
-		_.bindAll( this, 'hide' );
 		this.render();
 	},
 	render: function(){
 		this.$el.html( intro_template() );
 	},
-	hide: function( e ){
+	clickGotIt: function( e ){
+		console.log('clickind');
 		e.preventDefault();
+		this.remove();
 	}
 });
